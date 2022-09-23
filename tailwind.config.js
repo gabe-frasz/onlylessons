cfc427f40217ccbc36e9810b208e5920dff52217
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-23 20:56:55 -0300

    refactor(tailwind config): change fontFamily and colors values, remove daisyui plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,15 +11,39 @@ module.exports = {
         center: true,
         padding: "1rem",
       },
+      fontFamily: {
+        sans: "Roboto, sans-serif",
+      },
+      colors: {
+        green: {
+          300: "#00B37E",
+          500: "#00875F",
+          700: "#015F43",
+        },
+        blue: {
+          500: "#FBA94C",
+        },
+        orange: {
+          500: "#FBA94C",
+        },
+        red: {
+          500: "#F75A68",
+        },
+        gray: {
+          100: "#E1E1E6",
+          200: "#C4C4CC",
+          300: "#8D8D99",
+          500: "#323238",
+          600: "#29292E",
+          700: "#121214",
+          900: "#09090A",
+        },
+      },
     },
   },
   plugins: [
     // require("@tailwindcss/forms"),
     // require("tailwind-scrollbar"),
     require("@tailwindcss/typography"),
-    require("daisyui"),
   ],
-  daisyui: {
-    themes: ["dark", "light"],
-  },
 };
