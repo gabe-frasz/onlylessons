dc86fbbc917ef7becf894533016bb67dd8dc7aec
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-23 21:56:44 -0300

    refactor(types): add type for gql response

--- core/types/types.ts
+++ core/types/types.ts
@@ -1,3 +1,14 @@
+// * queries responses
+export type GetLessonsQueryResponse = {
+  lessons: {
+    id: string;
+    title: string;
+    slug: string;
+    availableAt: string;
+    lessonType: "live" | "class";
+  }[];
+};
+
 // * contexts values
 export type ThemeContextValue = {
   appTheme: "light" | "dark";
