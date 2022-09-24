b45c124a29595c6439a93707b1bbe817e90d05cf
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-23 21:55:38 -0300

    feat(sidebar): add lessons gql query and Lesson component

--- components/modules/Sidebar/Sidebar.tsx
+++ components/modules/Sidebar/Sidebar.tsx
@@ -1,9 +1,28 @@
+import { useQuery } from "@apollo/client";
+import { Lesson } from "@components/widgets";
+import { GET_LESSONS_QUERY } from "@core/graphql/queries";
+import { GetLessonsQueryResponse } from "@core/types";
+
 export const Sidebar = () => {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
   return (
-    <aside className="w-[348px] p-6 bg-gray-700 border-l border-gray-600">
+    <aside className="w-[352px] p-6 bg-gray-700 border-l border-gray-600">
       <span className="mb-6 pb-6 block text-2xl font-bold border-b border-gray-500">
         Lessons timeline
       </span>
+
+      <div className="flex flex-col gap-8">
+        {data?.lessons.map((lesson) => (
+          <Lesson
+            key={lesson.id}
+            title={lesson.title}
+            slug={lesson.slug}
+            availableAt={new Date(lesson.availableAt)}
+            type={lesson.lessonType}
+          />
+        ))}
+      </div>
     </aside>
   );
 };
