4f1a1bc598efb6e75c5c29c1f8402941402f67d2
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-23 21:57:14 -0300

    fix(_app.tsx): add apollo provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
+import { ApolloProvider } from "@apollo/client";
+import { hygraph } from "@core/services";
 import type { AppProps } from "next/app";
 import "../public/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={hygraph}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
