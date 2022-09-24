bec008e5ec87160cc175efdd69e54cecdbd697f0
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-24 15:20:04 -0300

    refactor: set position fixed and add custom scrollbar to sidebar

--- components/modules/Header/Header.tsx
+++ components/modules/Header/Header.tsx
@@ -2,7 +2,7 @@ import { Logo } from "@components/widgets";
 
 export const Header = () => {
   return (
-    <header className="w-full py-5 flex justify-center items-center bg-gray-700 border-b border-gray-600">
+    <header className="fixed w-full py-5 flex justify-center items-center bg-gray-700 border-b border-gray-600 z-10">
       <Logo />
     </header>
   );
--- components/modules/Sidebar/Sidebar.tsx
+++ components/modules/Sidebar/Sidebar.tsx
@@ -7,12 +7,12 @@ export const Sidebar = () => {
   const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
 
   return (
-    <aside className="w-[352px] p-6 bg-gray-700 border-l border-gray-600">
+    <aside className="fixed right-2 w-[360px] h-full p-6 bg-gray-700 border-l border-gray-600">
       <span className="mb-6 pb-6 block text-2xl font-bold border-b border-gray-500">
         Lessons timeline
       </span>
 
-      <div className="flex flex-col gap-8">
+      <div className="pr-4 flex flex-col gap-8 h-full overflow-y-scroll scrollbar-thin scroll-m-4 scrollbar-thumb-gray-300 scrollbar-thumb-rounded-full">
         {data?.lessons.map((lesson) => (
           <Lesson
             key={lesson.id}
