500c5a5963090c742b02d070be8a0bb3064e0122
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-26 20:10:53 -0300

    refactor(gql queries): change old gql queries and ts types to the ones generated with codegen

--- core/types/types.ts
+++ core/types/types.ts
@@ -1,26 +1,3 @@
-// * queries responses
-export type GetLessonsQueryResponse = {
-  lessons: {
-    id: string;
-    title: string;
-    slug: string;
-    availableAt: string;
-    lessonType: "live" | "class";
-  }[];
-};
-export type GetLessonBySlugQueryResponse = {
-  lesson: {
-    title: string;
-    description: string;
-    videoId: string;
-    teacher: {
-      name: string;
-      bio: string;
-      avatarURL: string;
-    } | null;
-  };
-};
-
 // * contexts values
 export type ThemeContextValue = {
   appTheme: "light" | "dark";
