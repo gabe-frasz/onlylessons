7983e336b15b6ca1c26cbbf6429cb168cd10ff18
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-26 19:16:19 -0300

    refactor(lesson): update classNames to use classnames lib function, change bg color when active

--- components/widgets/Lesson/Lesson.tsx
+++ components/widgets/Lesson/Lesson.tsx
@@ -1,6 +1,8 @@
 import { LessonProps } from "@core/types";
+import cn from "classnames";
 import { format, isPast } from "date-fns";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { CheckCircle, Lock } from "phosphor-react";
 
 export const Lesson = (props: LessonProps) => {
@@ -9,6 +11,8 @@ export const Lesson = (props: LessonProps) => {
     props.availableAt,
     "EEEE' • 'MMMM' 'dd'th • 'kk'h'mm"
   );
+
+  const isLessonActive = useRouter().query.slug === props.slug;
 
   return (
     <Link
@@ -20,16 +24,30 @@ export const Lesson = (props: LessonProps) => {
       }}
     >
       <a
-        className={`${
-          isLessonAvailable ? "group" : "pointer-events-none brightness-50"
-        }`}
+        className={cn({
+          group: isLessonAvailable,
+          "pointer-events-none brightness-50": !isLessonAvailable,
+          "pointer-events-none": isLessonActive,
+        })}
       >
         <span className="text-gray-300">{availableDateFormatted}</span>
 
-        <div className="mt-2 p-4 border border-gray-500 rounded group-hover:border-green-500 transition-colors">
+        <div
+          className={cn(
+            "mt-2 p-4 border border-gray-500 rounded group-hover:border-green-500 transition-colors",
+            {
+              "bg-green-500": isLessonActive,
+            }
+          )}
+        >
           <header className="flex justify-between items-center">
             {isLessonAvailable ? (
-              <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+              <span
+                className={cn("flex items-center gap-2 text-sm font-medium", {
+                  "text-white": isLessonActive,
+                  "text-blue-500": !isLessonActive,
+                })}
+              >
                 <CheckCircle size={20} />
                 Content available
               </span>
@@ -40,12 +58,27 @@ export const Lesson = (props: LessonProps) => {
               </span>
             )}
 
-            <span className="px-1 py-0.5 text-xs text-white font-bold border border-green-300 rounded">
+            <span
+              className={cn(
+                "px-1 py-0.5 text-xs text-white font-bold border rounded",
+                {
+                  "border-white": isLessonActive,
+                  "border-green-300": !isLessonActive,
+                }
+              )}
+            >
               {props.type === "live" ? "LIVE" : "PRACTICAL CLASS"}
             </span>
           </header>
 
-          <strong className="mt-5 block text-gray-200">{props.title}</strong>
+          <strong
+            className={cn("mt-5 block", {
+              "text-white": isLessonActive,
+              "text-gray-200": !isLessonActive,
+            })}
+          >
+            {props.title}
+          </strong>
         </div>
       </a>
     </Link>
