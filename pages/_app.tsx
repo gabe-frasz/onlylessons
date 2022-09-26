9699573a9afae2b801955d5c630c96f3850d383d
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-26 19:21:48 -0300

    feat(_app.tsx): add SessionProvider to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import { ApolloProvider } from "@apollo/client";
 import { hygraph } from "@core/services";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../public/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={hygraph}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <SessionProvider session={pageProps.session}>
+      <ApolloProvider client={hygraph}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </SessionProvider>
   );
 }
 
