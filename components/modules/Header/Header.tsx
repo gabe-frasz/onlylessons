8490ebad38060353fef0d5a07753215512ebb7cc
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-26 19:14:30 -0300

    feat(header): create button for users to unsubscribe

--- components/modules/Header/Header.tsx
+++ components/modules/Header/Header.tsx
@@ -1,9 +1,22 @@
 import { Logo } from "@components/widgets";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 
 export const Header = () => {
+  const router = useRouter();
+
   return (
-    <header className="fixed w-full py-5 flex justify-center items-center bg-gray-700 border-b border-gray-600 z-10">
+    <header className="fixed w-full p-5  flex justify-center items-center bg-gray-700 border-b border-gray-600 z-10">
       <Logo />
+
+      <button
+        onClick={async () => {
+          await signOut();
+          router.push("/");
+        }}
+      >
+        Unsubscribe
+      </button>
     </header>
   );
 };
