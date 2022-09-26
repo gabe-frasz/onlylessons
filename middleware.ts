ea36c8513dc696137c3ebe128108b6633388e8f6
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-26 19:25:51 -0300

    feat(middleware): update middleware to verify authentication
    verify authentication and redirect user if necessary

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,42 @@
-import type { NextRequest } from "next/server";
+// import type { NextRequest } from "next/server";
+import { withAuth } from "next-auth/middleware";
 import { NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/event/lessons/test", request.url));
-}
+// export function middleware(req: NextRequest) {
+//   switch (req.nextUrl.pathname) {
+//     case "/event":
+//     case "/event/lessons":
+//       return NextResponse.redirect(new URL("/event/lessons/test", req.url));
+//   }
+// }
 
-export const config = {
-  matcher: ["/event", "/event/lessons"],
-};
+export default withAuth(
+  function middleware(req) {
+    const isAuthenticated = req.nextauth.token !== null;
+
+    switch (req.nextUrl.pathname) {
+      case "/event":
+      case "/event/lessons":
+        if (!isAuthenticated) {
+          return NextResponse.redirect(new URL("/", req.url));
+        }
+        return NextResponse.redirect(new URL("/event/lessons/test", req.url));
+
+      case "/":
+        if (isAuthenticated) {
+          return NextResponse.redirect(new URL("/event/lessons/test", req.url));
+        }
+    }
+
+    if (req.nextUrl.pathname.includes("/event/lessons/")) {
+      if (!isAuthenticated) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+    }
+  },
+  {
+    callbacks: {
+      authorized: () => true,
+    },
+  }
+);
