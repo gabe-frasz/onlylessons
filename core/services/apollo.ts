3e7f52a30fed352e29573fcbf30837d7a0f94e12
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-26 19:21:16 -0300

    refactor(apollo): change uri value to env variable

--- core/services/apollo.ts
+++ core/services/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const hygraph = new ApolloClient({
-  uri: "https://api-sa-east-1.hygraph.com/v2/cl6rdlg880apy01uq6k68et2t/master",
+  uri: process.env.NEXT_PUBLIC_HYGRAPH_CONTENT_API,
   cache: new InMemoryCache(),
 });
