3d00653ddb773cc55238223a3503fa371750e870
Gabriel <gabrielvitor.frasao@gmail.com>
2022-09-26 20:14:08 -0300

    fix(_app.tsx): fix session error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import { ApolloProvider } from "@apollo/client";
 import { hygraph } from "@core/services";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import "../public/styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={pageProps.session}>
       <ApolloProvider client={hygraph}>
